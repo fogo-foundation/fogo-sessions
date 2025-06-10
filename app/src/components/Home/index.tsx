2adc5c3957eb8bd9db1a2a18f4f3138fd5b7c9e4
Guillermo Bescos <g.bescos@yahoo.com>
2025-06-10 20:29:46 +0100

    fix

--- app/src/components/Home/index.tsx
+++ app/src/components/Home/index.tsx
@@ -7,7 +7,13 @@ import {
   WalletDisconnectButton,
   WalletMultiButton,
 } from "@/components/WalletButton";
-import { Ed25519Program, Keypair, PublicKey, SystemProgram, Transaction } from "@solana/web3.js";
+import {
+  Ed25519Program,
+  Keypair,
+  PublicKey,
+  SystemProgram,
+  Transaction,
+} from "@solana/web3.js";
 import { useCallback, useState } from "react";
 import { AnchorProvider, Program, Wallet } from "@coral-xyz/anchor";
 import sessionManagerIdl from "@/idl/session_manager.json";
@@ -15,7 +21,6 @@ import type { SessionManager } from "@/idl/session_manager";
 
 const SPONSOR = new PublicKey(process.env.NEXT_PUBLIC_SPONSOR_KEY!);
 const SOLANA_RPC = process.env.NEXT_PUBLIC_SOLANA_RPC!;
-
 
 const handleEnableTrading = async (
   sessionManagerProgram: Program<SessionManager>,
@@ -25,67 +30,84 @@ const handleEnableTrading = async (
   setStatus: (status: "success" | "error") => void,
   setLink: (link: string) => void,
 ) => {
-    setLoading(true);
-    const sessionKey = Keypair.generate();
-    const provider = sessionManagerProgram.provider;
+  setLoading(true);
+  const sessionKey = Keypair.generate();
+  const provider = sessionManagerProgram.provider;
 
-    // TODO: This should be a function
-    const message = new TextEncoder().encode(`Fogo Sessions:\nSigning this intent will allow this app to interact with your on-chain balances. Please make sure you trust this app and the domain in the message matches the domain you are signing from.\n\nsession_key: ${sessionKey.publicKey.toBase58()}\nnonce: ${sessionKey.publicKey.toBase58()}\ndomain: gasless-trading.vercel.app\nextra: extra`)
+  // TODO: This should be a function
+  const message = new TextEncoder().encode(
+    `Fogo Sessions:\nSigning this intent will allow this app to interact with your on-chain balances. Please make sure you trust this app and the domain in the message matches the domain you are signing from.\n\nsession_key: ${sessionKey.publicKey.toBase58()}\nnonce: ${sessionKey.publicKey.toBase58()}\ndomain: gasless-trading.vercel.app\nextra: extra`,
+  );
 
-    const intentSignature = await signMessage!(message);
+  const intentSignature = await signMessage!(message);
 
-    const intentInstruction = Ed25519Program.createInstructionWithPublicKey(
-      {
-        publicKey: publicKey!.toBytes(),
-        signature: intentSignature,
-        message: message,
-      }
-    )
+  const intentInstruction = Ed25519Program.createInstructionWithPublicKey({
+    publicKey: publicKey!.toBytes(),
+    signature: intentSignature,
+    message: message,
+  });
 
-    const space = 200; // TODO: Compute this dynamically
-    const systemInstruction = SystemProgram.createAccount({
-      fromPubkey: SPONSOR,
-      newAccountPubkey: sessionKey.publicKey,
-      lamports: await provider.connection.getMinimumBalanceForRentExemption(space),
-      space: space,
-      programId: sessionManagerProgram.programId,
-    })
+  const space = 200; // TODO: Compute this dynamically
+  const systemInstruction = SystemProgram.createAccount({
+    fromPubkey: SPONSOR,
+    newAccountPubkey: sessionKey.publicKey,
+    lamports:
+      await provider.connection.getMinimumBalanceForRentExemption(space),
+    space: space,
+    programId: sessionManagerProgram.programId,
+  });
 
-    const transaction = new Transaction().add(intentInstruction).add(systemInstruction).add(
-      await sessionManagerProgram.methods.startSession().accounts(
-        {sponsor: SPONSOR, session: sessionKey.publicKey}
-      ).instruction()
+  const transaction = new Transaction()
+    .add(intentInstruction)
+    .add(systemInstruction)
+    .add(
+      await sessionManagerProgram.methods
+        .startSession()
+        .accounts({ sponsor: SPONSOR, session: sessionKey.publicKey })
+        .instruction(),
     );
 
-    transaction.recentBlockhash = (await provider.connection.getLatestBlockhash()).blockhash;
-    transaction.feePayer =  SPONSOR;
-    transaction.partialSign(sessionKey);
+  transaction.recentBlockhash = (
+    await provider.connection.getLatestBlockhash()
+  ).blockhash;
+  transaction.feePayer = SPONSOR;
+  transaction.partialSign(sessionKey);
 
-    const response = await fetch('/api/sponsor_and_send', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body:  JSON.stringify({transaction: Buffer.from(transaction.serialize({requireAllSignatures: false})).toString("base64")}),
-    })
+  const response = await fetch("/api/sponsor_and_send", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      transaction: Buffer.from(
+        transaction.serialize({ requireAllSignatures: false }),
+      ).toString("base64"),
+    }),
+  });
 
-    const lastValidBlockHeight = await provider.connection.getSlot();
-    const { signature } = await response.json()
-    const confirmationResult = await provider.connection.confirmTransaction({signature, blockhash: transaction.recentBlockhash, lastValidBlockHeight})
-    if (confirmationResult.value.err === null) {  
-      setStatus("success")
-    } else {
-      setStatus("error")
-    }
-    setLink(`https://explorer.fogo.io/tx/${signature}?cluster=custom&customUrl=${SOLANA_RPC}`)
-    setLoading(false)
+  const lastValidBlockHeight = await provider.connection.getSlot();
+  const { signature } = await response.json();
+  const confirmationResult = await provider.connection.confirmTransaction({
+    signature,
+    blockhash: transaction.recentBlockhash,
+    lastValidBlockHeight,
+  });
+  if (confirmationResult.value.err === null) {
+    setStatus("success");
+  } else {
+    setStatus("error");
+  }
+  setLink(
+    `https://explorer.fogo.io/tx/${signature}?cluster=custom&customUrl=${SOLANA_RPC}`,
+  );
+  setLoading(false);
 };
 
 export const Home = () => {
   const { connection } = useConnection();
   const [loading, setLoading] = useState(false);
   const [link, setLink] = useState<string | null>(null);
-  const [status, setStatus] = useState<'success' | 'error' | null>(null);
+  const [status, setStatus] = useState<"success" | "error" | null>(null);
 
   const provider = new AnchorProvider(connection, {} as Wallet, {});
 
@@ -96,12 +118,17 @@ export const Home = () => {
 
   const onEnableTrading = useCallback(() => {
     if (signMessage && publicKey) {
-      handleEnableTrading(sessionManagerProgram, publicKey, signMessage, setLoading, setStatus, setLink).catch(
-        (error) => {
-          setLoading(false);
-          console.error(error);
-        },
-      );
+      handleEnableTrading(
+        sessionManagerProgram,
+        publicKey,
+        signMessage,
+        setLoading,
+        setStatus,
+        setLink,
+      ).catch((error) => {
+        setLoading(false);
+        console.error(error);
+      });
     }
   }, [publicKey, signMessage, sessionManagerProgram]);
 
@@ -119,7 +146,9 @@ export const Home = () => {
         )}
         {link && status && (
           <a href={link} target="_blank" rel="noopener noreferrer">
-            {status === 'success' ? '✅ View Transaction' : '❌ Transaction Failed'}
+            {status === "success"
+              ? "✅ View Transaction"
+              : "❌ Transaction Failed"}
           </a>
         )}
       </div>
