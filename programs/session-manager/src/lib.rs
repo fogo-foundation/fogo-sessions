1d996cc79bd2c122eb662ae0c7c9ae1f9faaef28
Guillermo Bescos <g.bescos@yahoo.com>
2025-06-10 19:05:15 +0100

    fix clippy

--- programs/session-manager/src/lib.rs
+++ programs/session-manager/src/lib.rs
@@ -1,7 +1,8 @@
+#![allow(unexpected_cfgs)] // warning: unexpected `cfg` condition value: `anchor-debug`
+#![allow(deprecated)] // warning: use of deprecated method `anchor_lang::prelude::AccountInfo::<'a>::realloc`: Use AccountInfo::resize() instead
 use anchor_lang::prelude::*;
 
 declare_id!("mCB9AkebGNqN7HhUPxisr7Hd8HzHifCpubj9dCwvctk");
-
 #[program]
 pub mod session_manager {
     use super::*;
