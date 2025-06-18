dbb9984f241f1a7c9e0f267d0b7c4740e0f363d8
Connor Prussin <connor@prussin.net>
2025-06-18 07:22:54 -0700

    feat: initialize @fogo/sessions-sdk package (#7)

---
+++ packages/sessions-sdk/integration-tests/index.test.ts
@@ -0,0 +1,40 @@
+import { exec } from "node:child_process";
+import { rm } from "node:fs/promises";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { promisify } from "node:util";
+
+const ONE_SECOND_IN_MS = 1000;
+
+const TEST_DIR = path.join(
+  path.dirname(fileURLToPath(import.meta.url)),
+  "__fixtures__",
+  "test-package",
+);
+const NODE_MODULES = path.join(TEST_DIR, "node_modules");
+
+const execAsync = promisify(exec);
+
+describe("integration", () => {
+  beforeEach(async () => {
+    await rm(NODE_MODULES, { recursive: true, force: true });
+  });
+
+  afterEach(async () => {
+    await rm(NODE_MODULES, { recursive: true });
+  });
+
+  it(
+    "installs & works",
+    async () => {
+      await execAsync("pnpm i", { cwd: TEST_DIR });
+      const { stdout } = await execAsync("node ./index.js", {
+        cwd: TEST_DIR,
+        // eslint-disable-next-line n/no-process-env
+        env: { ...process.env, FORCE_COLOR: "0" },
+      });
+      expect(stdout).toMatchSnapshot();
+    },
+    240 * ONE_SECOND_IN_MS,
+  );
+});
