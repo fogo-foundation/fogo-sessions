25e43ae51b1962dc5ad57b7e7dad5461cb8bbfea
Guillermo Bescos <g.bescos@yahoo.com>
2025-06-23 17:08:54 +0100

    improve comment

--- packages/sessions-sdk-rs/src/lib.rs
+++ packages/sessions-sdk-rs/src/lib.rs
@@ -43,7 +43,7 @@ pub struct SessionInfo {
     pub expiration: UnixTimestamp,
     /// Programs the session key is allowed to interact with as a (program_id, signer_pda) pair. We store the signer PDAs so we don't have to recalculate them
     pub authorized_programs: AuthorizedPrograms,
-    /// Tokens the session key is allowed to interact with. If `Specific`, the spend limits are stored in each individual token account.
+    /// Tokens the session key is allowed to interact with. If `Specific`, the spend limits are stored in each individual token account in the usual delegated_amount field.
     pub authorized_tokens: AuthorizedTokens,
     /// Extra (key, value)'s provided by the user
     pub extra: Extra,
