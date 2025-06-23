46197e619699c0273b6a9d0347107e2fdfd91544
Guillermo Bescos <g.bescos@yahoo.com>
2025-06-23 16:21:18 +0100

    does this work

--- packages/sessions-idls/src/idl/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as SessionManagerIdl } from "./session-manager.json" with { type: "json" };
-export { default as ExampleIdl } from "./example.json" with { type: "json" };
--- packages/sessions-idls/src/types/index.ts
+++
@@ -1,2 +0,0 @@
-export type { SessionManager } from "./session-manager.js";
-export type { Example } from "./example.js";
