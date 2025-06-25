b55004dccacbef59d24209753708e37f456a91a9
guibescos <59208140+guibescos@users.noreply.github.com>
2025-06-25 21:45:52 +0200

    fix: turbo lint (#16)
    * fix turbo lint
    
    * try another fix
    
    * add turbo

--- apps/sessions-demo/src/config/server.ts
+++ apps/sessions-demo/src/config/server.ts
@@ -17,7 +17,6 @@ function envOrThrow(name: string) {
 const defaultInDevelopment = (name: string, devValue: string) =>
   IS_DEV_MACHINE ? (process.env[name] ?? devValue) : envOrThrow(name);
 
-/* eslint-disable turbo/no-undeclared-env-vars */
 export const ADDRESS_LOOKUP_TABLE_ADDRESS = IS_DEV_MACHINE
   ? "93QGBU8ZHuvyKSvDFeETsdek1KQs4gqk3mEVKG8UxoX3"
   : process.env.ADDRESS_LOOKUP_TABLE_ADDRESS;
