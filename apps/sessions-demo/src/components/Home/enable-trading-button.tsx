4b8c63fd9e969d676e75fa6d175b1342bb5b96bf
Guillermo Bescos <g.bescos@yahoo.com>
2025-06-25 20:17:44 +0100

    fix

--- apps/sessions-demo/src/components/Home/enable-trading-button.tsx
+++ apps/sessions-demo/src/components/Home/enable-trading-button.tsx
@@ -41,13 +41,15 @@ const handleEnableTrading = async (
     }
 > => {
   const provider = sessionManagerProgram.provider;
+  const sessionKey = Keypair.generate();
+
+  const expires = new Date(Date.now() + 3600 * 1000).toISOString();
 
   const umi = createUmi(solanaRpc);
   const metaplexNativeMint = metaplexPublicKey(NATIVE_MINT.toBase58());
   const metadataAddress = findMetadataPda(umi, { mint: metaplexNativeMint })[0];
   const metadata = await fetchMetadata(umi, metadataAddress);
 
-  const sessionKey = Keypair.generate();
   // TODO: This should be a function
   const message = new TextEncoder().encode(
     `Fogo Sessions:
