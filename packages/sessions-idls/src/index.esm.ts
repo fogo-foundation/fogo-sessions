dfa845352622ad1d56dccf06a15e12d861a7a5d7
Guillermo Bescos <g.bescos@yahoo.com>
2025-06-30 13:51:40 +0100

    remove this file

--- packages/sessions-idls/src/index.esm.ts
+++
@@ -1,7 +0,0 @@
-export { default as SessionManagerIdl } from "./idl/session-manager.json" with { type: "json" };
-export { default as ExampleIdl } from "./idl/example.json" with { type: "json" };
-export { default as ChainIdIdl } from "./idl/chain-id.json" with { type: "json" };
-
-export type { SessionManager } from "./types/session-manager.js";
-export type { Example } from "./types/example.js";
-export type { ChainId } from "./types/chain-id.js";
