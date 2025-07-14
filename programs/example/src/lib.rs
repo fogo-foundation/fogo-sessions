3df19f9b2fbc51a7e8989b3535f3ec2dc184339c
Guillermo Bescos <g.bescos@yahoo.com>
2025-07-14 17:51:24 +0100

    rename to core

--- programs/example/src/lib.rs
+++ programs/example/src/lib.rs
@@ -2,17 +2,17 @@
 
 use anchor_lang::prelude::*;
 use anchor_spl::token::{Mint, Token, TokenAccount};
-use fogo_sessions_sdk_solana::{session::Session, token::PROGRAM_SIGNER_SEED};
+use fogo_sessions_sdk_core::{session::Session, token::PROGRAM_SIGNER_SEED};
+use anchor_lang::solana_program::program::invoke_signed;
 
 declare_id!("Examtz9qAwhxcADNFodNA2QpxK7SM9bCHyiaUvWvFBM3");
 
 #[program]
 pub mod example {
-    use spl_token::solana_program::program::invoke_signed;
 
     use super::*;
     pub fn example_transfer(ctx: Context<ExampleTransfer>, amount: u64) -> Result<()> {
-        let instruction = fogo_sessions_sdk_solana::token::instruction::transfer(
+        let instruction = fogo_sessions_sdk_core::token::instruction::transfer(
             &ctx.accounts.token_program.key,
             &ctx.accounts.user_token_account.key(),
             &ctx.accounts.sink.key(),
@@ -20,17 +20,14 @@ pub mod example {
             &ctx.accounts.cpi_signer.key(),
             amount,
         )?;
-
         invoke_signed(&instruction, &ctx.accounts.to_account_infos(), &[&[PROGRAM_SIGNER_SEED.as_ref(), &[ctx.bumps.cpi_signer]]])?;
-
-
         Ok(())
     }
 }
 
 #[derive(Accounts)]
 pub struct ExampleTransfer<'info> {
-    /// CHECK: check
+    /// CHECK: we check this using `Session::check_signer_or_session_key`
     #[account(signer)]
     pub session_key: AccountInfo<'info>,
     /// CHECK: this is just a signer for token program CPIs
@@ -43,15 +40,3 @@ pub struct ExampleTransfer<'info> {
     pub sink: Account<'info, TokenAccount>,
     pub token_program: Program<'info, Token>,
 }
-
-// impl<'info> ExampleTransfer<'info> {
-//     pub fn to_in_session_token_transfer_accounts(&self) -> InSessionTokenTransferAccounts<'info> {
-//         InSessionTokenTransferAccounts {
-//             source: self.user_token_account.to_account_info(),
-//             mint: self.mint.to_account_info(),
-//             destination: self.sink.to_account_info(),
-//             session_key: self.session_key.to_account_info(),
-//             cpi_signer: self.cpi_signer.to_account_info(),
-//         }
-//     }
-// }
