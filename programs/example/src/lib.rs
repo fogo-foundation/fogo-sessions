4090fc5c9567d9ddbfc8f8912d92580fec937c49
Guillermo Bescos <g.bescos@yahoo.com>
2025-07-16 18:34:54 +0100

    cooking

--- programs/example/src/lib.rs
+++ programs/example/src/lib.rs
@@ -18,7 +18,7 @@ pub mod example {
         // Extract the user public key from the signing account
         let user = Session::extract_user_from_signer_or_session(&ctx.accounts.signer_or_session, &crate::ID).map_err(|_| ProgramError::InvalidAccountData)?;
 
-        // Check that user_token_account is the user's token associated account
+        // Check that user_token_account is the user's associated token account
         require_eq!(get_associated_token_address(&user, &ctx.accounts.mint.key()), ctx.accounts.user_token_account.key());
 
         let instruction = transfer_checked(
@@ -59,7 +59,7 @@ pub struct ExampleTransfer<'info> {
     /// This is either the user or a session representing the user
     pub signer_or_session: Signer<'info>,
     /// If within a session, this account is needed to sign token transfers in addition to the session key.
-    /// CHECK: this is just a signer for token program CPIs
+    /// CHECK: this is just a PDA signer for token program CPIs
     #[account(seeds = [PROGRAM_SIGNER_SEED], bump)]
     pub program_signer: Option<AccountInfo<'info>>,
     #[account(mut, token::mint = mint)]
