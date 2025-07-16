ec9053a083edfbeb7c7fbba296d2a69607fa62b4
Guillermo Bescos <g.bescos@yahoo.com>
2025-07-16 18:29:42 +0100

    go

--- packages/sessions-sdk-rs/src/token/mod.rs
+++ packages/sessions-sdk-rs/src/token/mod.rs
@@ -1,5 +1,5 @@
 pub mod instruction;
 
-/// This seed is used to derive a program's signer PDA in the context of session's token transfers.
+/// This seed is used to derive a program's signer PDA in the context of sessions.
 /// This PDA is required to sign in-session token transfers. This allows the token program to verify that the transfer is happening within an authorized program.
 pub const PROGRAM_SIGNER_SEED: &[u8] = b"fogo_session_program_signer";
