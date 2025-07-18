2afce10838bdcc9840f2165c342c583e5933e036
Guillermo Bescos <g.bescos@yahoo.com>
2025-07-18 21:05:49 +0100

    format

--- services/paymaster/src/api.rs
+++ services/paymaster/src/api.rs
@@ -1,8 +1,7 @@
-use std::sync::Arc;
 use crate::config::Config;
 use axum::extract::State;
 use axum::http::StatusCode;
-use axum::response::{ErrorResponse};
+use axum::response::ErrorResponse;
 use axum::Json;
 use axum::{
     http::{HeaderName, Method},
@@ -14,11 +13,12 @@ use solana_client::rpc_config::RpcSendTransactionConfig;
 use solana_keypair::Keypair;
 use solana_signer::Signer;
 use solana_transaction::versioned::VersionedTransaction;
+use std::sync::Arc;
 use tower_http::cors::{AllowHeaders, AllowMethods, AllowOrigin, CorsLayer};
 use utoipa_axum::{router::OpenApiRouter, routes};
 
 pub struct ServerState {
-    pub url : String,
+    pub url: String,
     pub keypair: Keypair,
 }
 
@@ -32,15 +32,33 @@ pub struct SponsorAndSendPayload {
     path = "/sponsor_and_send",
     request_body = SponsorAndSendPayload,
 )]
-async fn sponsor_and_send_handler(State(state): State<Arc<ServerState>>, Json(payload): Json<SponsorAndSendPayload>) -> Result<String, ErrorResponse> {
+async fn sponsor_and_send_handler(
+    State(state): State<Arc<ServerState>>,
+    Json(payload): Json<SponsorAndSendPayload>,
+) -> Result<String, ErrorResponse> {
     let rpc = RpcClient::new(state.url.clone());
     let engine = base64::engine::general_purpose::STANDARD;
-    
-    let transaction_bytes = engine.decode(&payload.transaction)
+
+    let transaction_bytes = engine
+        .decode(&payload.transaction)
         .map_err(|_| StatusCode::BAD_REQUEST)?;
-    let mut transaction: VersionedTransaction = bincode::deserialize(&transaction_bytes).map_err(|e| (StatusCode::BAD_REQUEST, "Failed to deserialize transaction"))?;
+    let mut transaction: VersionedTransaction = bincode::deserialize(&transaction_bytes)
+        .map_err(|e| (StatusCode::BAD_REQUEST, "Failed to deserialize transaction"))?;
     transaction.signatures[0] = state.keypair.sign_message(&transaction.message.serialize());
-    let signature = rpc.send_transaction_with_config(&transaction, RpcSendTransactionConfig {skip_preflight: true, ..RpcSendTransactionConfig::default()}).map_err(|err| (StatusCode::INTERNAL_SERVER_ERROR, format!("Failed to sponsor and send: {}", err)))?;
+    let signature = rpc
+        .send_transaction_with_config(
+            &transaction,
+            RpcSendTransactionConfig {
+                skip_preflight: true,
+                ..RpcSendTransactionConfig::default()
+            },
+        )
+        .map_err(|err| {
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Failed to sponsor and send: {}", err),
+            )
+        })?;
     Ok(signature.to_string())
 }
 
@@ -49,17 +67,22 @@ pub async fn run_server(config: Config) -> () {
         .routes(routes!(sponsor_and_send_handler))
         .split_for_parts();
 
-    let app = Router::new().nest("/api", router).layer(
-        CorsLayer::new()
-            .allow_origin(AllowOrigin::any())
-            .allow_methods(AllowMethods::exact(Method::POST))
-            .allow_headers(AllowHeaders::list(vec![HeaderName::from_static(
-                "content-type",
-            )])),
-    )        .with_state(Arc::new(ServerState {
-        url: config.url,
-        keypair: config.keypair,
-    }));;
-    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", config.port)).await.unwrap();
+    let app = Router::new()
+        .nest("/api", router)
+        .layer(
+            CorsLayer::new()
+                .allow_origin(AllowOrigin::any())
+                .allow_methods(AllowMethods::exact(Method::POST))
+                .allow_headers(AllowHeaders::list(vec![HeaderName::from_static(
+                    "content-type",
+                )])),
+        )
+        .with_state(Arc::new(ServerState {
+            url: config.url,
+            keypair: config.keypair,
+        }));
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", config.port))
+        .await
+        .unwrap();
     axum::serve(listener, app).await.unwrap();
 }
--- services/paymaster/src/main.rs
+++ services/paymaster/src/main.rs
@@ -1,30 +1,30 @@
+use crate::config::Config;
 use clap::Parser;
 use solana_keypair::Keypair;
 use solana_signer::EncodableKey;
-use crate::config::Config;
 
-mod api; 
+mod api;
 mod config;
 
 #[derive(Parser)]
 struct Cli {
     #[clap(short, long, default_value = "./tilt/keypairs/sponsor.json")]
-    keypair_path : String,
+    keypair_path: String,
     #[clap(short, long, default_value = "http://127.0.0.1:8899")]
-    url : String,
+    url: String,
     #[clap(short, long, default_value = "4000")]
-    port : u16
+    port: u16,
 }
 
 #[tokio::main]
 async fn main() -> () {
     let cli = Cli::parse();
     let keypair = Keypair::read_from_file(&cli.keypair_path).unwrap();
-    
+
     api::run_server(Config {
         keypair,
         url: cli.url,
         port: cli.port,
-    }).await
+    })
+    .await
 }
-
