0a8c140b1b787efb88c06b1fd54afb7d513e5ca9
Guillermo Bescos <g.bescos@yahoo.com>
2025-07-18 14:46:57 +0100

    go

--- programs/session-manager/src/lib.rs
+++ programs/session-manager/src/lib.rs
@@ -64,7 +64,7 @@ pub mod session_manager {
         Ok(())
     }
 
-    /// This is currently just to trick anchor into generating the IDL for the Session account
+    /// This is just to trick anchor into generating the IDL for the Session account since we don't use it in the context for `start_session`
     #[instruction(discriminator = [1])]
     pub fn _unused<'info>(
         _ctx: Context<'_, '_, '_, 'info, Unused<'info>>,
