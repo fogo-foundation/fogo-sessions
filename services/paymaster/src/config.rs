3b1f0653884721a970b09b30e010ddcafaba0214
guibescos <59208140+guibescos@users.noreply.github.com>
2025-07-21 22:25:48 +0200

    faet: add program whitelist (#72)
    * add program whitelist
    
    * fix pubkey
    
    * go
    
    * done
    
    * fmt
    
    * fmt
    
    * fix error thingy
    
    * cluppy
    
    * fmt
    
    * program whitelist

--- services/paymaster/src/config.rs
+++ services/paymaster/src/config.rs
@@ -1,7 +1,33 @@
-use solana_keypair::Keypair;
+use anyhow::Result;
+use config::File;
+use serde::{Deserialize, Deserializer};
+use solana_pubkey::Pubkey;
+use std::str::FromStr;
 
+fn deserialize_pubkey_vec<'de, D>(deserializer: D) -> Result<Vec<Pubkey>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let strings: Vec<String> = Vec::deserialize(deserializer)?;
+    strings
+        .into_iter()
+        .map(|s| Pubkey::from_str(&s).map_err(serde::de::Error::custom))
+        .collect()
+}
+
+#[derive(Deserialize)]
 pub struct Config {
-    pub keypair: Keypair,
-    pub url: String,
+    pub keypair_path: String,
+    pub solana_url: String,
     pub listen_address: String,
+    #[serde(deserialize_with = "deserialize_pubkey_vec")]
+    pub program_whitelist: Vec<Pubkey>,
+}
+
+pub fn load_config(config_path: &str) -> Result<Config> {
+    let config: Config = config::Config::builder()
+        .add_source(File::with_name(config_path))
+        .build()?
+        .try_deserialize()?;
+    Ok(config)
 }
--- services/paymaster/src/main.rs
+++ services/paymaster/src/main.rs
@@ -1,30 +1,19 @@
-use crate::config::Config;
+use crate::config::load_config;
 use clap::Parser;
-use solana_keypair::Keypair;
-use solana_signer::EncodableKey;
 
 mod api;
 mod config;
 
 #[derive(Parser)]
 struct Cli {
-    #[clap(short, long, default_value = "./tilt/keypairs/sponsor.json")]
-    keypair_path: String,
-    #[clap(short, long, default_value = "http://127.0.0.1:8899")]
-    url: String,
-    #[clap(short, long, default_value = "0.0.0.0:4000")]
-    listen_address: String,
+    #[clap(short, long, default_value = "./tilt/configs/paymaster.toml")]
+    config: String,
 }
 
 #[tokio::main]
 async fn main() {
     let cli = Cli::parse();
-    let keypair = Keypair::read_from_file(&cli.keypair_path).unwrap();
+    let config = load_config(&cli.config).unwrap();
 
-    api::run_server(Config {
-        keypair,
-        url: cli.url,
-        listen_address: cli.listen_address,
-    })
-    .await
+    api::run_server(config).await
 }
