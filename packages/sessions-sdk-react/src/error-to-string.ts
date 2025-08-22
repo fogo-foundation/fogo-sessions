bd6a7d9d0276c69d6940e9eb3ed3b3129cd96efa
Guillermo Bescos <g.bescos@yahoo.com>
2025-08-22 16:18:15 +0100

    fix: handle object errors

--- packages/sessions-sdk-react/src/error-to-string.ts
+++ packages/sessions-sdk-react/src/error-to-string.ts
@@ -3,6 +3,8 @@ export const errorToString = (error: unknown) => {
     return error.message;
   } else if (typeof error === "string") {
     return error.toString();
+  } else if (typeof error === "object") {
+    return JSON.stringify(error);  
   } else {
     return "Unknown Error";
   }
