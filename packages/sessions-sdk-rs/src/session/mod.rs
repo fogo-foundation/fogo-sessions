98820c70fcc7290959da7706defd5724c94c0e5c
Guillermo Bescos <g.bescos@yahoo.com>
2025-09-15 17:05:53 +0100

    fix: remove lint reasons as they are not compatible with old versions of rust

--- packages/sessions-sdk-rs/src/session/mod.rs
+++ packages/sessions-sdk-rs/src/session/mod.rs
@@ -53,11 +53,9 @@ pub struct Session {
     pub session_info: SessionInfo,
 }
 
-#[allow(
-    dead_code,
-    reason = "This module is a hack because the BorshSchema macro generates dead code for `SessionInfo` in this version of borsh, but we don't want to disable dead_code globally.
+/// This module is a hack because the BorshSchema macro generates dead code for `SessionInfo` in this version of borsh, but we don't want to disable dead_code globally.
 /// More info: https://github.com/near/borsh-rs/issues/111"
-)]
+#[allow(dead_code)]
 mod session_info {
     use super::*;
 
@@ -70,11 +68,9 @@ mod session_info {
 }
 pub use session_info::SessionInfo;
 
-#[allow(
-    dead_code,
-    reason = "This module is a hack because the BorshSchema macro generates dead code for `V2` in this version of borsh, but we don't want to disable dead_code globally.
+/// This module is a hack because the BorshSchema macro generates dead code for `V2` in this version of borsh, but we don't want to disable dead_code globally.
 /// More info: https://github.com/near/borsh-rs/issues/111"
-)]
+#[allow(dead_code)]
 mod v2 {
     use super::*;
     #[derive(Debug, Clone, BorshDeserialize, BorshSerialize, BorshSchema)]
@@ -101,11 +97,9 @@ pub struct ActiveSessionInfo {
     pub extra: Extra,
 }
 
-#[allow(
-    dead_code,
-    reason = "This module is a hack because the BorshSchema macro generates dead code for `AuthorizedPrograms` in this version of borsh, but we don't want to disable dead_code globally.
+///This module is a hack because the BorshSchema macro generates dead code for `AuthorizedPrograms` in this version of borsh, but we don't want to disable dead_code globally.
 /// More info: https://github.com/near/borsh-rs/issues/111"
-)]
+#[allow(dead_code)]
 mod authorized_programs {
     use super::*;
 
