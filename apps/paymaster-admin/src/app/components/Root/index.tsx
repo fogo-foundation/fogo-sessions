7bac75eb993be96ce1220fe83f8f5c1808c620d2
alexcambose <alexcambose@yahoo.com>
2025-10-08 16:10:49 +0100

    feat: add paymaster admin project

---
+++ apps/paymaster-admin/src/app/components/Root/index.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+
+export const Root = ({ children }: Props) => (
+  <html lang="en">
+    <body>{children}</body>
+  </html>
+);
