a6b335d91e4a5a8cd077f5321372b335ec20b41c
alexcambose <alexcambose@yahoo.com>
2025-10-09 11:35:54 +0100

    formatting

--- apps/paymaster-admin/next.config.js
+++ apps/paymaster-admin/next.config.js
@@ -1,4 +1,3 @@
-
 const config = {
   reactStrictMode: true,
 
@@ -8,34 +7,34 @@ const config = {
     },
   },
 
-  headers: () => Promise.resolve([
-    {
-      source: "/:path*",
-      headers: [
-        {
-          key: "X-XSS-Protection",
-          value: "1; mode=block",
-        },
-        {
-          key: "Referrer-Policy",
-          value: "strict-origin-when-cross-origin",
-        },
-        {
-          key: "Strict-Transport-Security",
-          value: "max-age=2592000",
-        },
-        {
-          key: "X-Content-Type-Options",
-          value: "nosniff",
-        },
-        {
-          key: "Permissions-Policy",
-          value:
-            "vibrate=(), geolocation=(), midi=(), notifications=(), push=(), sync-xhr=(), microphone=(), camera=(), magnetometer=(), gyroscope=(), speaker=(), vibrate=(), fullscreen=self",
-        },
+  headers: () =>
+    Promise.resolve([
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=2592000",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Permissions-Policy",
+            value:
+              "vibrate=(), geolocation=(), midi=(), notifications=(), push=(), sync-xhr=(), microphone=(), camera=(), magnetometer=(), gyroscope=(), speaker=(), vibrate=(), fullscreen=self",
+          },
         ],
       },
-    ],
-  ),
+    ]),
 };
 export default config;
