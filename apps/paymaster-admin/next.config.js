ada7a0ab5a684f609741b2ce6d7b709de6a3682b
Alexandru Cambose <alexcambose@yahoo.com>
2025-10-10 12:28:04 +0100

    feat: add paymaster admin project (#212)
    * feat: add paymaster admin project
    
    * PR review
    
    * formatting

---
+++ apps/paymaster-admin/next.config.js
@@ -0,0 +1,40 @@
+const config = {
+  reactStrictMode: true,
+
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+
+  headers: () =>
+    Promise.resolve([
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=2592000",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Permissions-Policy",
+            value:
+              "vibrate=(), geolocation=(), midi=(), notifications=(), push=(), sync-xhr=(), microphone=(), camera=(), magnetometer=(), gyroscope=(), speaker=(), vibrate=(), fullscreen=self",
+          },
+        ],
+      },
+    ]),
+};
+export default config;
