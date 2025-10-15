2273056e0d4ac9f6bb184e15270fbd67c36fa71c
guibescos <59208140+guibescos@users.noreply.github.com>
2025-10-15 15:08:25 +0100

    feat: tollbooth program (#228)
    * checkpoint
    
    * clippy
    
    * fmt
    
    * add test
    
    * fmt
    
    * add changeset
    
    * add tollbooth idl program
    
    * use macro
    
    * grind a better key
    
    * rename domain id to domain hash
    
    * is_live_and_unrevoked
    
    * bump rust sdk
    
    * rename
    
    * rename
    
    * smol
    
    * do

---
+++ programs/tollbooth/src/error.rs
@@ -0,0 +1,11 @@
+use anchor_lang::prelude::*;
+
+#[error_code]
+pub enum TollboothError {
+    #[msg(
+        "The source token account is not the associated token account of the user of the session"
+    )]
+    InvalidSource,
+    #[msg("The destination token account is not the associated token account of the toll recipient for the domain")]
+    InvalidDestination,
+}
