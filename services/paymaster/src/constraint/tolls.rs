62cf8ea72d450c188844a742699d2d89ec15046b
Guillermo Bescos <g.bescos@yahoo.com>
2025-10-16 14:02:28 +0100

    add amount

--- services/paymaster/src/constraint/tolls.rs
+++ services/paymaster/src/constraint/tolls.rs
@@ -1,6 +1,7 @@
 use borsh::BorshDeserialize;
 use fogo_sessions_sdk::tollbooth::TOLLBOOTH_PROGRAM_ID;
 use reqwest::StatusCode;
+use solana_program::instruction::CompiledInstruction;
 use solana_pubkey::Pubkey;
 use solana_transaction::versioned::VersionedTransaction;
 use crate::constraint::get_instruction_account_pubkey_by_index;
@@ -31,55 +32,50 @@ impl Tolls {
     const MINT_ACCOUNT_INDEX_PAY_TOLL_INSTRUCTION: usize = 4;
 
     pub async fn validate_toll_payment(&self, transaction: &VersionedTransaction, chain_index: &ChainIndex) -> Result<(), (StatusCode, String)> {
-        match self {
-            Tolls::Free => Ok(()),
-            Tolls::Fixed(tolls) => {
-                let tollbooth_instructions = transaction.message.instructions().iter().enumerate().filter(|(index, ix)| {
-                    ix.program_id(transaction.message.static_account_keys()) == &TOLLBOOTH_PROGRAM_ID
-                });
-            
-                match tollbooth_instructions.collect::<Vec<_>>()[..] {
-                    [(index, tollbooth_instruction)] => {
-                        #[derive(BorshDeserialize)]
-                        enum TollboothInstruction {
-                            PayToll(u64),
-                        }
-                        
-                        let tollbooth_instruction_data = TollboothInstruction::try_from_slice(
-                            &tollbooth_instruction.data,
+        let tollbooth_instructions : Vec<(usize, &CompiledInstruction)> = transaction.message.instructions().iter().enumerate().filter(|(_, ix)| {
+            ix.program_id(transaction.message.static_account_keys()) == &TOLLBOOTH_PROGRAM_ID
+        }).collect();
+
+        match (self, tollbooth_instructions.as_slice()) {
+            (Tolls::Free, []) => Ok(()),
+            (Tolls::Free, _) => Err((
+                StatusCode::BAD_REQUEST,
+                "The paymaster for this domain does not require a toll payment".to_string(),
+            )),
+            (Tolls::Fixed(tolls), [(index, tollbooth_instruction)]) => {
+                    #[derive(BorshDeserialize)]
+                    enum TollboothInstruction {
+                        PayToll(u64),
+                    }
+                    
+                    let tollbooth_instruction_data = TollboothInstruction::try_from_slice(
+                        &tollbooth_instruction.data,
+                    )
+                    .map_err(|_| {
+                        (
+                            StatusCode::BAD_REQUEST,
+                            "Tollbooth instruction data is not valid".to_string(),
                         )
-                        .map_err(|_| {
-                            (
-                                StatusCode::BAD_REQUEST,
-                                "Tollbooth instruction data is not valid".to_string(),
-                            )
-                        })?;
-                        let TollboothInstruction::PayToll(amount) = tollbooth_instruction_data;
-                        let mint = get_instruction_account_pubkey_by_index(transaction, tollbooth_instruction, index, Self::MINT_ACCOUNT_INDEX_PAY_TOLL_INSTRUCTION, chain_index).await?;
+                    })?;
+                    let TollboothInstruction::PayToll(amount) = tollbooth_instruction_data;
+                    let mint = get_instruction_account_pubkey_by_index(transaction, tollbooth_instruction, *index, Self::MINT_ACCOUNT_INDEX_PAY_TOLL_INSTRUCTION, chain_index).await?;
 
-                        if tolls.iter().any(|toll| toll.mint == mint && toll.amount <= amount) {
-                            return Ok(());
-                        }
-                        return Err((
-                            StatusCode::BAD_REQUEST,
-                            "Payment toll is less than the minimum or not the correct mint".to_string(),
-                        ));
-
+                    if tolls.iter().any(|toll| toll.mint == mint && toll.amount == amount) {
+                        return Ok(());
                     }
-                    [] => {
-                        return Err((
-                            StatusCode::BAD_REQUEST,
-                            "Paymaster tolls are required for this domain".to_string(),
-                        ))
-                    }
-                    _ => {
-                        return Err((    
-                            StatusCode::BAD_REQUEST,
-                            "Multiple paymaster tolls instructionsfound".to_string(),
-                        ))
-                    }
-                }
-            }
+                    return Err((
+                        StatusCode::BAD_REQUEST,
+                        "Payment toll is less than the minimum or not the correct mint".to_string(),
+                    ));
+            },
+            (Tolls::Fixed(_), []) => Err((
+                StatusCode::BAD_REQUEST,
+                "The paymaster for this domain requires a toll payment".to_string(),
+            )),
+            (Tolls::Fixed(_), [_, ..]) => Err((
+                StatusCode::BAD_REQUEST,
+                "This transaction contains multiple tollbooth instructions".to_string(),
+            )),
         }
     }
 }
