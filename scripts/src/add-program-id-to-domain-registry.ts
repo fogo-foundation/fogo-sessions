01ef6159b04c7141c1bf9173800371ccde3a2489
Connor Prussin <connor@prussin.net>
2025-11-09 13:55:14 -0800

    feat: add script to register ntt manager
    This commit also includes some cleanup for all of the scripts

--- scripts/src/add-program-id-to-domain-registry.ts
+++ scripts/src/add-program-id-to-domain-registry.ts
@@ -1,71 +1,28 @@
-import fs from "node:fs";
-
-import { AnchorProvider, Wallet } from "@coral-xyz/anchor";
 import { DomainRegistryProgram } from "@fogo/sessions-idls";
 import { getDomainRecordAddress } from "@fogo/sessions-sdk";
-import { Connection, Keypair, PublicKey } from "@solana/web3.js";
+import { PublicKey } from "@solana/web3.js";
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 
-import { LedgerNodeWallet, parseDerivationPath } from "./ledger.js";
-
-async function parseSignerSource(source: string) {
-  if (source.startsWith("usb://ledger")) {
-    return await LedgerNodeWallet.create(parseDerivationPath(source));
-  } else {
-    const keypair = Keypair.fromSecretKey(
-      Buffer.from(JSON.parse(fs.readFileSync(source, "utf8"))),
-    );
-    return new Wallet(keypair);
-  }
-}
+import { anchorOptions, createAnchorProvider } from "./anchor-options.js";
 
 export const main = async (argv: string[] = hideBin(process.argv)) => {
   const args = await yargs(argv)
-    .option("url", {
-      alias: "u",
-      type: "string",
-      description: "URL for Solana's JSON RPC or moniker",
-    })
-    .option("keypair", {
-      alias: "k",
-      type: "string",
-      description: "Filepath to a keypair",
-    })
+    .command("* <domain> <program-id>", "command")
+    .options(anchorOptions)
     .positional("domain", {
       type: "string",
       description: "Domain to update with the program id",
+      demandOption: true,
     })
     .positional("program-id", {
       type: "string",
       description: "Program ID to add to the domain",
+      demandOption: true,
     })
     .parse();
 
-  if (!args.url) {
-    throw new Error("Missing required argument: url");
-  }
-
-  if (!args.keypair) {
-    throw new Error("Missing required argument: keypair");
-  }
-
-  if (!args.domain) {
-    throw new Error("Missing required argument: domain");
-  }
-
-  if (!args["program-id"]) {
-    throw new Error("Missing required argument: program-id");
-  }
-
-  const url = args.url == "l" ? "http://localhost:8899" : args.url;
-
-  const connection = new Connection(url);
-  const provider = new AnchorProvider(
-    connection,
-    await parseSignerSource(args.keypair),
-  );
-  const program = new DomainRegistryProgram(provider);
+  const program = new DomainRegistryProgram(await createAnchorProvider(args));
 
   const { config: configPubkey } = await program.methods.initialize().pubkeys();
 
---
+++ scripts/src/anchor-options.ts
@@ -0,0 +1,29 @@
+import { AnchorProvider } from "@coral-xyz/anchor";
+import { Connection } from "@solana/web3.js";
+
+import { parseSignerSource } from "./ledger.js";
+
+export const anchorOptions = {
+  url: {
+    alias: "u",
+    type: "string",
+    description: "URL for Solana's JSON RPC or moniker",
+    coerce: (url: string) => (url === "l" ? "http://localhost:8899" : url),
+    demandOption: true,
+  },
+  keypair: {
+    alias: "k",
+    type: "string",
+    description: "Filepath to a keypair",
+    demandOption: true,
+    coerce: (keypair: string) => parseSignerSource(keypair),
+  },
+} as const;
+
+export const createAnchorProvider = async ({
+  url,
+  keypair,
+}: {
+  url: string;
+  keypair: ReturnType<typeof parseSignerSource>;
+}) => new AnchorProvider(new Connection(url), await keypair);
