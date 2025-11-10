7c2af31299a5ac10aa28983025caefbdb00cf0c2
Connor Prussin <connor@prussin.net>
2025-11-10 12:53:12 -0800

    feat: add script to register ntt manager (#272)
    * feat: add script to register ntt manager
    
    This commit also includes some cleanup for all of the scripts
    
    * PR feedback
    
    * More PR feedback

--- scripts/src/initialize-chain-id.ts
+++ scripts/src/initialize-chain-id.ts
@@ -1,48 +1,21 @@
-import fs from "node:fs";
-
-import { AnchorProvider, Wallet } from "@coral-xyz/anchor";
 import { ChainIdProgram } from "@fogo/sessions-idls";
-import { Connection, Keypair } from "@solana/web3.js";
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 
+import { anchorOptions, createAnchorProvider } from "./anchor-options.js";
+
 export const main = async (argv: string[] = hideBin(process.argv)) => {
   const args = await yargs(argv)
-    .option("url", {
-      alias: "u",
-      type: "string",
-      description: "URL for Solana's JSON RPC or moniker",
-    })
-    .option("keypair", {
-      alias: "k",
-      type: "string",
-      description: "Filepath to a keypair",
-    })
+    .command("* <chain-id>", "Set the chain ID")
+    .options(anchorOptions)
     .positional("chain-id", {
       type: "string",
       description: "Chain ID to set as a string",
+      demandOption: true,
     })
     .parse();
 
-  if (!args.url) {
-    throw new Error("Missing required argument: url");
-  }
-
-  if (!args.keypair) {
-    throw new Error("Missing required argument: keypair");
-  }
-
-  if (!args["chain-id"]) {
-    throw new Error("Missing required argument: chain-id");
-  }
-
-  const url = args.url == "l" ? "http://localhost:8899" : args.url;
-
-  const connection = new Connection(url);
-  const keypair = Keypair.fromSecretKey(
-    Buffer.from(JSON.parse(fs.readFileSync(args.keypair, "utf8"))),
-  );
-  const provider = new AnchorProvider(connection, new Wallet(keypair));
-  const program = new ChainIdProgram(provider);
-  await program.methods.set(args["chain-id"]).rpc();
+  await new ChainIdProgram(createAnchorProvider(args)).methods
+    .set(args["chain-id"])
+    .rpc();
 };
