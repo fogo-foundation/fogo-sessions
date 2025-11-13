b84977c1ba8cc41755f6d04af81200d8c0bfcfbd
Alexandru Cambose <alexcambose@yahoo.com>
2025-11-13 09:27:58 +0100

    fix: variation mapping (#267)
    * fix: variation mapping
    
    * fix
    
    * fix

--- services/paymaster/src/db/config.rs
+++ services/paymaster/src/db/config.rs
@@ -33,16 +33,25 @@ pub async fn load_config() -> Result<Config, sqlx::Error> {
     .fetch_all(pool::pool())
     .await?;
 
-    let map: HashMap<Uuid, Domain> = rows.into_iter().fold(HashMap::new(), |mut acc, r| {
-        let domain = Domain {
-            domain: r.domain,
-            enable_session_management: r.enable_session_management,
-            enable_preflight_simulation: r.enable_preflight_simulation,
-            tx_variations: vec![r.transaction_variation.0],
-        };
-        acc.insert(r.domain_id, domain);
-        acc
-    });
+    let mut map: HashMap<Uuid, Domain> = HashMap::new();
+
+    for Row {
+        domain_id,
+        domain,
+        enable_session_management,
+        enable_preflight_simulation,
+        transaction_variation,
+    } in rows
+    {
+        let domain_ref = map.entry(domain_id).or_insert_with(|| Domain {
+            domain,
+            enable_session_management,
+            enable_preflight_simulation,
+            tx_variations: Vec::new(),
+        });
+
+        domain_ref.tx_variations.push(transaction_variation.0);
+    }
 
     Ok(Config {
         domains: map.into_values().collect(),
