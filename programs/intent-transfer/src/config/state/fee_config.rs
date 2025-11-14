f2bc7ec58f89bb1ff16a4614ff29174e466bda0d
Guillermo Bescos <g.bescos@yahoo.com>
2025-11-14 23:16:14 +0000

    clippy

--- programs/intent-transfer/src/config/state/fee_config.rs
+++ programs/intent-transfer/src/config/state/fee_config.rs
@@ -1,9 +1,4 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token::spl_token::try_ui_amount_into_amount;
-use anchor_spl::token::{transfer_checked, Mint, Token, TokenAccount, TransferChecked};
-use solana_intents::SymbolOrMint;
-
-use crate::{error::IntentTransferError, verify::verify_symbol_or_mint};
 
 pub const FEE_CONFIG_SEED: &[u8] = b"fee_config";
 
