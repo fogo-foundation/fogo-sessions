7cf0dfce0a8c2fe878b66d5507562f04e7314ec5
Connor Prussin <connor@prussin.net>
2025-11-25 08:26:00 -0800

    finish update-metadata script

--- scripts/ts/src/update-metadata.ts
+++ scripts/ts/src/update-metadata.ts
@@ -9,12 +9,12 @@ import {
 } from "@metaplex-foundation/umi";
 import { base58 } from "@metaplex-foundation/umi/serializers";
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import { fromWeb3JsKeypair } from "@metaplex-foundation/umi-web3js-adapters";
+import { PublicKey } from "@solana/web3.js";
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
-import {fromWeb3JsKeypair} from "@metaplex-foundation/umi-web3js-adapters";
 
 import { anchorOptions, createAnchorProvider } from "./anchor-options.js";
-import { PublicKey } from "@solana/web3.js";
 
 export const main = async (argv: string[] = hideBin(process.argv)) => {
   const args = await yargs(argv)
@@ -22,7 +22,21 @@ export const main = async (argv: string[] = hideBin(process.argv)) => {
       "* <mint>",
       "Update the Metaplex metadata for the provided mint address",
     )
-    .options(anchorOptions)
+    .options({
+      ...anchorOptions,
+      name: {
+        type: "string",
+        description: "Token name",
+      },
+      symbol: {
+        type: "string",
+        description: "Token symbol",
+      },
+      tokenUri: {
+        type: "string",
+        description: "Token metadata uri",
+      },
+    })
     .positional("mint", {
       type: "string",
       description: "Mint to set the metadata for",
@@ -31,35 +45,38 @@ export const main = async (argv: string[] = hideBin(process.argv)) => {
     })
     .parse();
 
-  const provider = createAnchorProvider(args)
+  const provider = createAnchorProvider(args);
 
-  const umi  = createUmi(provider.connection.rpcEndpoint);
-  umi.use(
-    keypairIdentity(fromWeb3JsKeypair(provider.wallet.payer!),true)
-  );
+  const umi = createUmi(provider.connection.rpcEndpoint);
+  if (provider.wallet.payer === undefined) {
+    throw new Error("Wallet has no payer");
+  }
+  umi.use(keypairIdentity(fromWeb3JsKeypair(provider.wallet.payer), true));
   const mint = umiPublicKey(args.mint);
   const metadataPda = findMetadataPda(umi, { mint })[0];
   const metadata = await safeFetchMetadata(umi, metadataPda);
 
   if (metadata === null) {
-    throw new Error(`No metadata account found for mint ${args.mint}.`);
+    throw new Error(
+      `No metadata account found for mint ${args.mint.toBase58()}.`,
+    );
   }
-
-  const data =
-  { name: "USDC",
-   symbol: "USDC.s",
-   uri:"https://arweave.net/nE8Yxx_CJmiSZvYWv8qS-SGG2yw-UEIkDa1pGmVraks",
-   sellerFeeBasisPoints: 0,
-   creators: null,
- }
 
   const { signature } = await updateV1(umi, {
     mint,
     metadata: metadataPda,
     authority: umi.identity,
-    data,
+    data: {
+      name: args.name ?? metadata.name,
+      symbol: args.symbol ?? metadata.symbol,
+      uri: args.tokenUri ?? metadata.uri,
+      sellerFeeBasisPoints: 0,
+      // eslint-disable-next-line unicorn/no-null
+      creators: null,
+    },
   }).sendAndConfirm(umi);
 
   const signatureString = base58.deserialize(signature)[0];
+  // eslint-disable-next-line no-console
   console.log(`Transaction signature: ${signatureString}`);
 };
