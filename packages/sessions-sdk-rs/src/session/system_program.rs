c7ca70c07d7f7f0730ea947a421101539b2f4b34
Guillermo Bescos <g.bescos@yahoo.com>
2025-12-09 23:53:20 +0000

    add some comments

--- packages/sessions-sdk-rs/src/session/system_program.rs
+++ packages/sessions-sdk-rs/src/session/system_program.rs
@@ -6,6 +6,9 @@ use borsh::{BorshDeserialize, BorshSchema, BorshSerialize};
 use solana_program::pubkey::Pubkey;
 use solana_program::sysvar::clock::Clock;
 
+/// For the system program, we don't care about the authorized_tokens, authorized_program or extra fields, but we
+/// care about deserializing the session account taking roughly the same 
+/// amount of compute no matter the contents, so we replace the usual ActiveSessionInfo struct with this one.
 #[derive(Debug, Clone, BorshDeserialize, BorshSerialize, BorshSchema)]
 pub struct ActiveSessionInfo<T: IsAuthorizedTokens> {
     pub user: Pubkey,
@@ -19,6 +22,7 @@ impl Session {
     /// It doesn't check if the session is authorized to interact with the system program, this is because a session is always authorized to interact with the system program,
     /// since all sessions can do via the system program is wrap tokens for the user.
     /// We need pass the clock as an argumenthere because 'Clock::get' can't be called in a native program.
+    /// DO NOT USE THIS FUNCTION IN SBF PROGRAMS and use `Session::extract_user_from_signer_or_session` or `Session::get_user_checked` instead.
     pub fn get_user_checked_system_program(&self, clock: &Clock) -> Result<Pubkey, SessionError> {
         self.check_is_live_and_unrevoked_with_clock(clock)?;
         Ok(*self.user()?)
