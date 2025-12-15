f2a14661533d82510334b137bdd38769afd294b7
Alexandru Cambose <alexcambose@yahoo.com>
2025-12-15 15:39:27 +0200

    Component library storybook (#428)
    * chore: delete old component library
    
    * feat: set up component library
    
    This commit initializes the component library package.  So that we don't have to
    publish it or have it as a separate package dependency of the react sdk, it is
    symlinked to the react sdk src and is distributed along with the react sdk code.
    With this approach, components that are in the component library but are not in
    use by the react sdk will be included in the built package, however they won't
    be accessible to consumers due to subpath exports.
    
    * lockfile
    
    * add storybook
    
    * lockfile
    
    * revert
    
    * format
    
    * ignore stories
    
    * add next
    
    * add vercel.json
    
    ---------
    
    Co-authored-by: Connor Prussin <connor@prussin.net>

---
+++ packages/component-library/src/Link/index.stories.tsx
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Link as LinkComponent } from "./index.jsx";
+
+const meta = {
+  component: LinkComponent,
+  argTypes: {
+    children: {
+      control: "text",
+      table: {
+        category: "Contents",
+      },
+    },
+    isDisabled: {
+      control: "boolean",
+      table: {
+        category: "State",
+      },
+    },
+    isPending: {
+      control: "boolean",
+      table: {
+        category: "State",
+      },
+    },
+  },
+} satisfies Meta<typeof LinkComponent>;
+export default meta;
+
+export const Link = {
+  args: {
+    children: "Link",
+    href: "https://www.fogo.io",
+    target: "_blank",
+    isDisabled: false,
+  },
+} satisfies StoryObj<typeof LinkComponent>;
