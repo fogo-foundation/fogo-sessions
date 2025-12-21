b4735ec8c3f0eec400454a52101795c88d8dccda
Alexandru Cambose <alexcambose@yahoo.com>
2025-12-21 23:36:16 +0200

    feat(component-library): card component

---
+++ packages/component-library/src/Card/index.tsx
@@ -0,0 +1,14 @@
+import clsx from "clsx";
+import type { ComponentProps } from "react";
+
+import styles from "./index.module.css";
+
+type CardProps = ComponentProps<"div">;
+
+export const Card = ({ children, className, ...props }: CardProps) => {
+  return (
+    <div className={clsx(styles.card, className)} {...props}>
+      {children}
+    </div>
+  );
+};
