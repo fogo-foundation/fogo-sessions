505ef2d940d656dd5f3528729138f6d9f50146ff
Alexandru Cambose <alexcambose@yahoo.com>
2025-12-22 17:55:30 +0200

    Update packages/component-library/src/Card/index.tsx
    Co-authored-by: Connor Prussin <connor@prussin.net>

--- packages/component-library/src/Card/index.tsx
+++ packages/component-library/src/Card/index.tsx
@@ -7,10 +7,6 @@ import styles from "./index.module.css";
 
 type CardProps = ComponentProps<"div">;
 
-export const Card = ({ children, className, ...props }: CardProps) => {
-  return (
-    <div className={clsx(styles.card, className)} {...props}>
-      {children}
-    </div>
-  );
-};
+export const Card = ({ className, ...props }: CardProps) => (
+  <div className={clsx(styles.card, className)} {...props} />
+);
