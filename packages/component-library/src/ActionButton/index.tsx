29c8564b005976627b20a14b13670a02109c9f28
Alexandru Cambose <alexcambose@yahoo.com>
2025-12-22 19:33:37 +0200

    feat(component-library): Action button (#448)
    * feat(component-library): Action button
    
    * formatting
    
    * Apply suggestion from @cprussin
    
    Co-authored-by: Connor Prussin <connor@prussin.net>
    
    * Update index.tsx
    
    ---------
    
    Co-authored-by: Connor Prussin <connor@prussin.net>

---
+++ packages/component-library/src/ActionButton/index.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import clsx from "clsx";
+import type { ComponentProps, ReactNode } from "react";
+import { Toolbar, Button as UnstyledButton } from "react-aria-components";
+
+import styles from "./index.module.css";
+
+type ActionButtonProps = Omit<
+  ComponentProps<typeof UnstyledButton>,
+  "children"
+> & {
+  icon?: ReactNode;
+  children: ReactNode;
+};
+
+export const ActionButton = ({
+  className,
+  icon,
+  children,
+  ...props
+}: ActionButtonProps) => (
+  <UnstyledButton className={clsx(styles.actionButton, className)} {...props}>
+    {icon && <span className={styles.icon}>{icon}</span>}
+    <span className={styles.text}>{children}</span>
+  </UnstyledButton>
+);
+
+export const ActionButtonToolbar = ({
+  className,
+  ...props
+}: ComponentProps<typeof Toolbar>) => (
+  <Toolbar className={clsx(styles.actionButtonToolbar, className)} {...props} />
+);
