f185b9c28d2fd70447124e6a9ea6403c9213d811
Alexandru Cambose <alexcambose@yahoo.com>
2026-01-05 19:11:45 +0200

    feat(component-library): badge

---
+++ packages/component-library/src/Badge/index.stories.tsx
@@ -0,0 +1,49 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Badge as BadgeComponent, SIZES, STYLES, VARIANTS } from "./index.jsx";
+
+const meta = {
+  component: BadgeComponent,
+  globals: {
+    backgrounds: { value: "dark" },
+  },
+  argTypes: {
+    children: {
+      control: "text",
+      table: {
+        category: "Contents",
+      },
+    },
+    variant: {
+      control: "inline-radio",
+      options: VARIANTS,
+      table: {
+        category: "Variant",
+      },
+    },
+    style: {
+      control: "inline-radio",
+      options: STYLES,
+      table: {
+        category: "Variant",
+      },
+    },
+    size: {
+      control: "inline-radio",
+      options: SIZES,
+      table: {
+        category: "Variant",
+      },
+    },
+  },
+} satisfies Meta<typeof BadgeComponent>;
+export default meta;
+
+export const Badge = {
+  args: {
+    children: "A BADGE",
+    variant: "neutral",
+    style: "filled",
+    size: "md",
+  },
+} satisfies StoryObj<typeof BadgeComponent>;
---
+++ packages/component-library/src/Badge/index.tsx
@@ -0,0 +1,33 @@
+import clsx from "clsx";
+import type { ComponentProps } from "react";
+
+import styles from "./index.module.css";
+
+export const VARIANTS = ["neutral", "error", "success"] as const;
+export const STYLES = ["filled", "outline"] as const;
+export const SIZES = ["xs", "md", "lg"] as const;
+
+type Props = ComponentProps<"span"> & {
+  variant?: (typeof VARIANTS)[number] | undefined;
+  style?: (typeof STYLES)[number] | undefined;
+  size?: (typeof SIZES)[number] | undefined;
+};
+
+export const Badge = ({
+  className,
+  variant = "neutral",
+  size = "md",
+  style = "filled",
+  children,
+  ...props
+}: Props) => (
+  <span
+    className={clsx(styles.badge, className)}
+    data-variant={variant}
+    data-size={size}
+    data-style={style}
+    {...props}
+  >
+    {children}
+  </span>
+);
