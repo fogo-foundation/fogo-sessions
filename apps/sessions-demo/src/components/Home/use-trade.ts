6fed9cfdb04b018ff6372c088c02c95712b410b5
Connor Prussin <connor@prussin.net>
2026-01-10 08:38:21 -0800

    Revert "feat: support fees in the typescript sdks (#450)" (#503)
    This reverts commit c07b35487e9e47884644dc0be841cbc67918ecf3.

--- apps/sessions-demo/src/components/Home/use-trade.ts
+++ apps/sessions-demo/src/components/Home/use-trade.ts
@@ -10,8 +10,9 @@ import {
 } from "@solana/spl-token";
 import { PublicKey } from "@solana/web3.js";
 import { useCallback } from "react";
-import { useAsync } from "../../hooks/use-async";
+
 import type { Transaction } from "./use-transaction-log";
+import { useAsync } from "../../hooks/use-async";
 
 export const useTrade = (
   sessionState: EstablishedSessionState,
@@ -28,28 +29,25 @@ export const useTrade = (
     );
     const { decimals } = await getMint(connection, mint);
 
-    const result = await sessionState.sendTransaction(
-      [
-        createAssociatedTokenAccountIdempotentInstruction(
-          sessionState.payer,
-          sinkAta,
-          sessionState.payer,
+    const result = await sessionState.sendTransaction([
+      createAssociatedTokenAccountIdempotentInstruction(
+        sessionState.payer,
+        sinkAta,
+        sessionState.payer,
+        mint,
+      ),
+      await new ExampleProgram(
+        new AnchorProvider(connection, {} as Wallet, {}),
+      ).methods
+        .exampleTransfer(new BN(amount * Math.pow(10, decimals)))
+        .accountsPartial({
+          signerOrSession: sessionState.sessionPublicKey,
+          sink: sinkAta,
+          userTokenAccount,
           mint,
-        ),
-        await new ExampleProgram(
-          new AnchorProvider(connection, {} as Wallet, {}),
-        ).methods
-          .exampleTransfer(new BN(amount * Math.pow(10, decimals)))
-          .accountsPartial({
-            signerOrSession: sessionState.sessionPublicKey,
-            sink: sinkAta,
-            userTokenAccount,
-            mint,
-          })
-          .instruction(),
-      ],
-      { variation: "Example v1 Variation" },
-    );
+        })
+        .instruction(),
+    ]);
 
     appendTransaction({
       description: "Trade",
