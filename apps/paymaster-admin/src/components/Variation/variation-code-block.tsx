23eed9c868ffce56b1dc3052db47cc88e172caec
Alexandru Cambose <alexcambose@yahoo.com>
2026-01-13 11:46:37 +0000

    code editor

---
+++ apps/paymaster-admin/src/components/Variation/variation-code-block.tsx
@@ -0,0 +1,109 @@
+import { Button } from "@fogo/component-library/Button";
+import {
+  ArrowsInIcon,
+  ArrowsOutIcon,
+  ChecksIcon,
+} from "@phosphor-icons/react/dist/ssr";
+import { AnimatePresence, motion } from "motion/react";
+import { useCallback, useRef, useState } from "react";
+import AceEditor from "react-ace";
+import type { Variation } from "../../db-schema";
+import styles from "./variation-code-block.module.scss";
+import "ace-builds/src-noconflict/theme-monokai";
+import "./ace-theme.scss";
+import { Badge } from "@fogo/component-library/Badge";
+import { useToast } from "@fogo/component-library/Toast";
+
+export const VariationCodeBlock = ({
+  isExpanded,
+  variation,
+}: {
+  isExpanded: boolean;
+  variation: Variation;
+}) => {
+  const toast = useToast();
+
+  const [code, setCode] = useState("");
+  const [isFullscreen, setIsFullscreen] = useState(false);
+  const cardRef = useRef<HTMLDivElement>(null);
+  const contentRef = useRef<HTMLDivElement>(null);
+  const [editorHeight, setEditorHeight] = useState(0);
+
+  const handleFullscreen = useCallback(() => {
+    if (document.fullscreenElement) {
+      document.exitFullscreen();
+      setIsFullscreen(false);
+      return;
+    }
+    // Otherwise enter fullscreen mode
+    cardRef.current
+      ?.requestFullscreen()
+      .then(() => {
+        setIsFullscreen(true);
+      })
+      .catch((err) => {
+        toast.error(`Error enabling fullscreen: ${err.message}`);
+      });
+  }, [isFullscreen, toast.error]);
+
+  const handleSave = useCallback(() => {
+    // todo
+    toast.success("Saved successfully!");
+  }, [toast.success]);
+
+  const handleUpdate = useCallback(() => {
+    setEditorHeight(contentRef.current?.clientHeight ?? 0);
+  }, []);
+
+  return (
+    <AnimatePresence>
+      {isExpanded && (
+        <motion.div
+          initial={{ height: 0, scale: 0.95 }}
+          animate={{ height: "50vh", scale: 1 }}
+          exit={{ height: 0, scale: 0.95 }}
+          className={styles.variationCodeBlock}
+          ref={cardRef}
+          onUpdate={handleUpdate}
+        >
+          <div className={styles.variationCodeBlockHeader}>
+            <h2 className={styles.variationCodeBlockHeaderTitle}>
+              Edit Configuration
+            </h2>
+            <Button variant="outline" onClick={handleFullscreen}>
+              {isFullscreen ? "Exit Fullscreen" : "Fullscreen"}
+              {isFullscreen ? <ArrowsOutIcon /> : <ArrowsInIcon />}
+            </Button>
+          </div>
+          <div className={styles.variationCodeBlockContent} ref={contentRef}>
+            {contentRef.current && (
+              <AceEditor
+                name={variation.id}
+                className={styles.variationCodeBlockEditor}
+                value={code}
+                onChange={setCode}
+                mode="javascript"
+                theme="monokai"
+                width="100%"
+                height={`${editorHeight}px`}
+                showPrintMargin={false}
+              />
+            )}
+          </div>
+          <div className={styles.variationCodeBlockFooter}>
+            <div className={styles.variationCodeBlockFooterInfo}>
+              <Badge variant="success" size="xs">
+                All passed <ChecksIcon />
+              </Badge>
+            </div>
+            <div className={styles.variationCodeBlockFooterButtons}>
+              <Button variant="secondary" onClick={handleSave}>
+                Save
+              </Button>
+            </div>
+          </div>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+};
--- apps/paymaster-admin/src/components/Variation/variations-list-item.tsx
+++ apps/paymaster-admin/src/components/Variation/variations-list-item.tsx
@@ -1,8 +1,14 @@
 import { Badge } from "@fogo/component-library/Badge";
+import { Button } from "@fogo/component-library/Button";
 import { Skeleton } from "@fogo/component-library/Skeleton";
 import { TextField } from "@fogo/component-library/TextField";
+import { CodeBlockIcon } from "@phosphor-icons/react/dist/ssr";
+import { GasPumpIcon } from "@phosphor-icons/react/dist/ssr/GasPump";
+import { AnimatePresence, motion } from "motion/react";
+import { useCallback, useState } from "react";
 import type { Variation } from "../../db-schema";
 import { DeleteVariationButton } from "./delete-variation-button";
+import { VariationCodeBlock } from "./variation-code-block";
 import styles from "./variations-list-item.module.scss";
 
 type VariationListItemProps =
@@ -15,19 +21,37 @@ type VariationListItemProps =
     };
 
 export const VariationListItem = (props: VariationListItemProps) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const handleExpand = useCallback(() => {
+    setIsExpanded((value) => !value);
+  }, []);
+
   return props.isLoading ? (
     <Skeleton className={styles.variationListItemSkeleton} />
   ) : (
-    <div className={styles.variationListItem}>
-      <div className={styles.variationListCard}>
-        <VariationVersionBadge version={props.variation.version} />
-        <TextField
-          value={props.variation.name}
-          className={styles.fieldVariationName ?? ""}
-        />
-        <TextField value={props.variation.max_gas_spend.toString()} />
+    <div>
+      <div className={styles.variationListItem}>
+        <div
+          className={styles.variationListCard}
+          data-is-expanded={isExpanded ? "" : undefined}
+        >
+          <VariationVersionBadge version={props.variation.version} />
+          <TextField
+            value={props.variation.name}
+            className={styles.fieldVariationName ?? ""}
+          />
+          <TextField
+            value={props.variation.max_gas_spend.toString()}
+            rightExtra={<GasPumpIcon />}
+          />
+          <Button variant="outline" onClick={handleExpand}>
+            <CodeBlockIcon />
+          </Button>
+        </div>
+        <DeleteVariationButton />
       </div>
-      <DeleteVariationButton />
+      <VariationCodeBlock isExpanded={isExpanded} variation={props.variation} />
     </div>
   );
 };
