471514d7f8a6a689120544e551d3b1fe47a53c07
Guillermo Bescos <g.bescos@yahoo.com>
2026-01-13 16:39:01 +0000

    go

--- packages/solana-intents/src/key_value.rs
+++ packages/solana-intents/src/key_value.rs
@@ -190,7 +190,7 @@ mod tests {
         fn test_empty_value_after_newline_with_next_key() {
             let result = key_value_with_key_type::<_, String, Error<&str>, _, _>(alphanumeric1)
                 .parse("foo:\nbaz");
-            assert!(result.is_ok());
+            assert_eq!(result, Ok(("", ("foo", "baz".to_string()))));
         }
 
         #[test]
