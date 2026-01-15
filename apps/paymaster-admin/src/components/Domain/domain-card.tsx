95de0134d0e7eb4d075a22671c37b14cfce69a37
Ben Duran <stratodyne@gmail.com>
2026-01-15 18:17:05 +0000

    fix: enforce type imports / exports being separate from code imports / exports. also fixed one occurrence of this :) (#525)
    * fix: enforce type imports / exports being separate from code imports / exports. also fixed one occurrence of this :)
    
    * fix: updated biome rule to separate the imports and corrected all offending files
    
    * chore: suppress noconsole
    
    * chore: added changeset

--- apps/paymaster-admin/src/components/Domain/domain-card.tsx
+++ apps/paymaster-admin/src/components/Domain/domain-card.tsx
@@ -1,10 +1,11 @@
-import { Badge, type BadgeProps } from "@fogo/component-library/Badge";
+import type { BadgeProps } from "@fogo/component-library/Badge";
+import { Badge } from "@fogo/component-library/Badge";
 import { Button } from "@fogo/component-library/Button";
 import { Card } from "@fogo/component-library/Card";
 import { Skeleton } from "@fogo/component-library/Skeleton";
-import z from "zod";
-import {
-  type DomainConfigWithVariationsSchema,
+import type z from "zod";
+import type {
+  DomainConfigWithVariationsSchema,
   NetworkEnvironmentSchema,
 } from "../../db-schema";
 import styles from "./domain-card.module.scss";
