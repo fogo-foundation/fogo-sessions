412d1a4633582db007ac03cf48a5af31b5548b8b
Alexandru Cambose <alexcambose@yahoo.com>
2026-01-27 14:18:14 +0000

    Remove max_gas_spend field from db schema

--- apps/paymaster-admin/src/db-schema.ts
+++ apps/paymaster-admin/src/db-schema.ts
@@ -76,7 +76,6 @@ export const VariationV0Schema = z.object({
   version: z.literal("v0"),
   name: z.string(),
   transaction_variation: z.array(Base58Pubkey),
-  max_gas_spend: z.null(),
   created_at: TimeStr,
   updated_at: TimeStr,
 });
