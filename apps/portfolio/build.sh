5b1533cab496b8c62c410ecdb8681bc52b994d6e
Alexandru Cambose <alexcambose@yahoo.com>
2026-02-02 16:47:17 +0000

    feat(paymaster-admin): domain setting update logic  (#563)
    * start domain settings
    
    * refactor(paymaster-admin): domain settings update logic
    
    * formatting
    
    * update query
    
    * nit

--- apps/portfolio/build.sh
+++ apps/portfolio/build.sh
@@ -2,6 +2,4 @@
 
 set -e
 
-. "$HOME/.cargo/env"
-cd ../..
 turbo run build --filter={apps/portfolio}...
--- apps/portfolio/install.sh
+++ apps/portfolio/install.sh
@@ -1,12 +1,2 @@
 #!/bin/sh
-
-set -e
-
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --profile minimal --default-toolchain nightly
-. "$HOME/.cargo/env"
-
-# Install systemd-devel for libudev
-dnf install -y systemd-devel
-cargo install --git https://github.com/coral-xyz/anchor --tag v0.31.1 anchor-cli
-
-pnpm i
+echo "OK"
