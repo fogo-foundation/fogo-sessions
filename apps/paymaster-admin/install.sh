185e1eee223b1ad062d52cf43f2531fbf81c35db
Connor Prussin <connor@prussin.net>
2026-02-03 12:42:17 -0800

    fix: Fix build (#573)

--- apps/paymaster-admin/install.sh
+++ apps/paymaster-admin/install.sh
@@ -2,11 +2,8 @@
 
 set -e
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --profile minimal --default-toolchain nightly
-. "$HOME/.cargo/env"
-
 # Install systemd-devel for libudev
 dnf install -y systemd-devel
-cargo install --git https://github.com/coral-xyz/anchor --tag v0.31.1 anchor-cli
+cargo install --git https://github.com/coral-xyz/anchor --tag v0.31.1 anchor-cli --locked
 
 pnpm i
