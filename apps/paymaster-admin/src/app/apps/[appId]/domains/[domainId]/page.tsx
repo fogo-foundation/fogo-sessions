48ca09aef3c2d565cabd430650934f2893dd42fa
Alexandru Cambose <alexcambose@yahoo.com>
2026-02-06 17:15:16 +0000

    fix(paymaster-admin): fix params after submission (#582)
    * log
    
    * this-made-me-hate-nextjs
    
    * remove log
    
    * fix
    
    * fix

--- apps/paymaster-admin/src/app/apps/[appId]/domains/[domainId]/page.tsx
+++ apps/paymaster-admin/src/app/apps/[appId]/domains/[domainId]/page.tsx
@@ -1,12 +1,18 @@
-import { Suspense } from "react";
-import { PaymasterLoading } from "../../../../../components/loading";
 import { Variation } from "../../../../../components/Variation";
 
-export default function DomainIdPage() {
-  // suspense is required here because of useParams()
-  return (
-    <Suspense fallback={<PaymasterLoading />}>
-      <Variation />
-    </Suspense>
-  );
+export default function DomainIdPage({
+  params,
+}: {
+  params: Promise<{ appId: string; domainId: string }>;
+}) {
+  return <VariationWithParams params={params} />;
+}
+
+async function VariationWithParams({
+  params,
+}: {
+  params: Promise<{ appId: string; domainId: string }>;
+}) {
+  const { appId, domainId } = await params;
+  return <Variation appId={appId} domainConfigId={domainId} />;
 }
--- apps/paymaster-admin/src/app/apps/[appId]/page.tsx
+++ apps/paymaster-admin/src/app/apps/[appId]/page.tsx
@@ -1,13 +1,18 @@
-import { Suspense } from "react";
-
 import { Domain } from "../../../components/Domain";
-import { PaymasterLoading } from "../../../components/loading";
 
-export default function AppIdPage() {
-  // suspense is required here because of useParams()
-  return (
-    <Suspense fallback={<PaymasterLoading />}>
-      <Domain />
-    </Suspense>
-  );
+export default function AppIdPage({
+  params,
+}: {
+  params: Promise<{ appId: string }>;
+}) {
+  return <DomainWithParams params={params} />;
+}
+
+async function DomainWithParams({
+  params,
+}: {
+  params: Promise<{ appId: string }>;
+}) {
+  const { appId } = await params;
+  return <Domain appId={appId} />;
 }
